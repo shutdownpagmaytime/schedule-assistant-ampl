4fd9dc8fb843798e962fd85f79fe8ea5716a03c9
shutdownpagmaytime <84158907+shutdownpagmaytime@users.noreply.github.com>
2021-05-21 06:46:02 +0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
-  if(req.url == '/app/')
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
 }).listen(port);
